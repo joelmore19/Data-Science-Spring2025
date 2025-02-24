ae727d2e930ad0a646194a9ec813b1f2a4bdb571
joelmore19 <111890592+joelmore19@users.noreply.github.com>
2025-02-24 12:40:32 -0500

    Add files via upload

---
+++ ANOVA-Test.R
@@ -0,0 +1,10 @@
+#Author: Joshua Elmore, Date: 02/24/2025, Purpose: To test ANOVA function
+
+#Importing dummy dataset in R
+PATH <- "https://raw.githubusercontent.com/guru99-edu/R-Programming/master/poisons.csv"
+
+# Create a hypothesis
+#Treatment types are effective against all poison types/there is a significance difference between treatment and poisson types
+
+# Read the dataset in a variable
+df <- read.csv(PATH)
