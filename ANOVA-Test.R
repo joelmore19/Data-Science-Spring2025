5dd3d0d2968710c323d933912cf4f3a454574b82
joelmore19 <111890592+joelmore19@users.noreply.github.com>
2025-02-26 12:44:38 -0500

    Add files via upload

--- ANOVA-Test.R
+++ ANOVA-Test.R
@@ -8,3 +8,6 @@ PATH <- "https://raw.githubusercontent.com/guru99-edu/R-Programming/master/poiso
 
 # Read the dataset in a variable
 df <- read.csv(PATH)
+
+#Understand levels/classes in your datasets
+levels(factor(df$treat))
