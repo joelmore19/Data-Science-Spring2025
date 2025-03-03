f850795e87bef1fcf0b86410056430337bbd7fa4
joelmore19 <111890592+joelmore19@users.noreply.github.com>
2025-03-03 12:34:47 -0500

    Add files via upload

--- ANOVA-Test.R
+++ ANOVA-Test.R
@@ -11,3 +11,13 @@ df <- read.csv(PATH)
 
 #Understand levels/classes in your datasets
 levels(factor(df$treat))
+
+#Applying the ANOVA Function
+anova_one_way <- aov(time~poison, data = df)
+summary(anova_one_way)
+
+Df Sum Sq Mean Sq F value   Pr(>F)    
+poison       1 0.9316  0.9316   20.67 3.96e-05 ***
+Residuals   46 2.0735  0.0451                     
+---
+Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
